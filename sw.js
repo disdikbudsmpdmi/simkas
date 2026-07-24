cce097efa6d63b4bfa05a955cf26cb1220d5b9ad
disdikbud <disdikbud.smp.dumai@gmail.com>
2026-07-24 19:22:26 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * karena diambil live dari Apps Script + Google Sheets.
  */
 
-const CACHE_NAME = 'simkas-cache-v4';
+const CACHE_NAME = 'simkas-cache-v5';
 const APP_SHELL = [
   './',
   './index.html',
